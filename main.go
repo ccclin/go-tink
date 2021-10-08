36ff27e1c0837c9600bcf52d3f46edbecfb59e63
ccclin <demeter.yeh@gmail.com>
2021-10-08 17:29:37 +0800

    init

---
+++ main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+
+	"github.com/google/tink/go/aead"
+	"github.com/google/tink/go/core/registry"
+	"github.com/google/tink/go/integration/gcpkms"
+	"github.com/google/tink/go/keyset"
+)
+
+const (
+	// Change this. AWS KMS, Google Cloud KMS and HashiCorp Vault are supported out of the box.
+	// like "gcp-kms://projects/xxxxxx/locations/us-central1/keyRings/xxxxxx/cryptoKeys/xxxxxx"
+	keyURI          = "gcp-kms://projects/...."
+	credentialsPath = "credentials.json"
+)
+
+func main() {
+	aad := []byte("Hello world")
+	kh, _ := keyset.NewHandle(aead.AES128GCMKeyTemplate())
+	// kh := getFromKMS("./key.json")
+	// createKeyJson("./key2.json", kh)
+	// kh2 := getFromKMS("./key2.json")
+
+	encode(aad, kh)
+	decode(aad, kh)
+}
+
+func createKeyJson(path string, kh *keyset.Handle) {
+	// Fetch the master key from a KMS.
+	gcpClient, err := gcpkms.NewClientWithCredentials(keyURI, credentialsPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	registry.RegisterKMSClient(gcpClient)
+	masterKey, err := gcpClient.GetAEAD(keyURI)
+	if err != nil {
+		log.Fatal(err)
+	}
+	f, _ := os.Create(path)
+	writer := keyset.NewJSONWriter(f)
+	defer f.Close()
+
+	err = kh.Write(writer, masterKey)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func getFromKMS(path string) *keyset.Handle {
+	// Fetch the master key from a KMS.
+	gcpClient, err := gcpkms.NewClientWithCredentials(keyURI, credentialsPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	registry.RegisterKMSClient(gcpClient)
+	masterKey, err := gcpClient.GetAEAD(keyURI)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	f, _ := os.Open(path)
+
+	reader := keyset.NewJSONReader(f)
+
+	kh, _ := keyset.Read(reader, masterKey)
+	return kh
+}
+
+func encode(aad []byte, kh *keyset.Handle) {
+	a, err := aead.New(kh)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fileBytes, _ := ioutil.ReadFile("./new.jpg")
+	ct, err := a.Encrypt(fileBytes, aad)
+	if err != nil {
+		log.Fatal(err)
+	}
+	ioutil.WriteFile("./output", ct, 0644)
+}
+
+func decode(aad []byte, kh *keyset.Handle) {
+	a, err := aead.New(kh)
+	if err != nil {
+		log.Fatal(err)
+	}
+	newFileBytes, _ := ioutil.ReadFile("./output")
+
+	pt, err := a.Decrypt(newFileBytes, aad)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ioutil.WriteFile("./new2.jpg", pt, 0644)
+}
